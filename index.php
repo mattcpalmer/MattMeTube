736dfb3bc959259e97835ae665618fac94fc98be
mpalme2 <mpalme2@g.clemson.edu>
2018-04-04 19:30:01 -0400

    m

--- index.php
+++ index.php
@@ -13,6 +13,19 @@
 	<script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>
 
     <title>METUBE</title>
+
+<style>
+center{
+
+text-align: center;
+
+}
+</style>
+
+
+
+
+
 </head>
 
 
@@ -80,6 +93,54 @@
 <!---  Display the Most Viewed Media --->
 <br>
 <h5> MOST VIEWED: </h5>
+
+<?php
+
+	$query = "SELECT * from media";
+	$result = mysql_query( $query );
+	if (!$result){
+	   die ("Could not query the media table in the database: <br />". mysql_error());
+	}
+?>
+
+    <div style="background:#339900;color:#FFFFFF; width:150px;">Uploaded Media</div>
+
+<table width="50%" cellpadding="0" cellspacing="0">
+  <?php
+    while ($result_row = mysql_fetch_row($result)) //filename, username, type, mediaid, path
+    {
+      $mediaid = $result_row[3];
+      $filename = $result_row[0];
+      $filenpath = $result_row[4];
+  ?>
+         <tr valign="top">
+    <td>
+        <?php
+          echo $mediaid;  //mediaid
+        ?>
+    </td>
+
+                        <a href="media.php?id=<?php echo $mediaid;?>" target="_blank"><?php echo $filename;?></a>
+
+                      <td>
+                        <a href="<?php echo $filenpath;?>" target="_blank"> <img src="<?php echo $filenpath;?>" alt="test" style="width:150:px"> <onclick="javascript:saveDownload(<?php echo $result_row[4];?>);">Download</a>
+                      </td>
+  </tr>
+        <?php
+    }
+  ?>
+</table>
+
+
+
+
+
+
+
+
+
+
+
 
 <hr>
 
