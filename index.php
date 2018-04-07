96df48010b55af91322ac001cf8200eea5bd655b
mpalme2 <mpalme2@g.clemson.edu>
2018-04-07 13:03:09 -0400

    j

--- index.php
+++ index.php
@@ -123,44 +123,25 @@ include_once "function.php";
     $filenpath = $result_row[4];
 ?>
 
+<!-- Display uploads -->
 
 <div class="row text-center">
 
             <div class="col-sm-4 col-md-4 col-lg-4 col-xs-6">
                 <div class = "panel panel-default">
-      			   <div class="img-thumbnail"> <a href="<?php echo $filenpath;?>"><img src="<?php echo $filenpath;?>" class="img-responsive" width = "400" height="200"></a></div>
-      			   <p>airship </p>
+      			   <div class="img-thumbnail"> <a href="<?php echo $filenpath;?>"><img src="<?php echo $filenpath;?>" class="img-responsive" width = "400" height="200"><onclick="javascript:saveDownload(<?php echo $result_row[4];?>);">Download</a></div>
+      			   <p> UploadName </p>
                     <h6>views: </h6>
                     <h6>upload date: </h6>
+                  <br>
+                  <br>
     		  </div>
             </div>
-        <!--    <div class="col-sm-4 col-md-4 col-lg-4 col-xs-6">
-                <div class = "panel panel-default">
-      			   <div class="img-thumbnail"> <a href="<?php echo $filenpath;?>"><img src="<?php echo $filenpath;?>" class="img-responsive" width = "400" height="200"></a></div>
-      			   <p>hello</p>
-                    <h6>views: </h6>
-                    <h6>uploads: </h6>
-    		  </div>
-        </div>  -->
-
 </div>
 
 <?php
 }
-
-  ?>
-
-
-
-
-
-
-
-
-
-
-
-
+?>
 
 <hr>
 
